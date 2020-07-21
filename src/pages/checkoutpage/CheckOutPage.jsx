10697a7a0fc3e5ad06af15878c034caeb7dd8db0
Pham Le Vi <vilepham136@gmail.com>
2020-07-22 03:51:00 +0900

    Update CheckOutPage.jsx

--- src/pages/checkoutpage/CheckOutPage.jsx
+++ src/pages/checkoutpage/CheckOutPage.jsx
@@ -39,13 +39,6 @@ const CheckOutPage = ({ cartItems, total, history }) => (
                 cartItems.length ?
                 (
                     <div>
-                        <div className='test-warning'>
-                            *Please use the following test credit card for payments*
-                            <br />
-                            4242 4242 4242 4242
-                            <br />
-                            Exp: 01/20 - CVV: 123
-                        </div>
                         <StripeCheckoutButton price={total}/>
                     </div>
                 ) : null
